4200d6271f0b405f783d7a2da14e2b5f487a028a
rlafranchi <rlafranchi@serenethemes.com>
2014-07-22 15:33:29 -0600

    sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
-  has_secure_password
-  validates :username, presence: true
+  has_secure_password validations: false
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: {minimum: 4}
 end
